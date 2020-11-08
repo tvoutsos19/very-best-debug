fd7d63d44a82a752d87913af75acf6a2271bce35
tvoutsos <tvoutsos@chicagobooth.edu>
2020-11-08 18:15:18 +0000

    Almost Done

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
 
-  get("/", { :controller => "users", :action => "home" })
-  get("/users", { :controller => "users", :action => "all_users" })
-  get("/users/:username", { :controller => "user", :action => "show" })
-  get("/insert_user_record", { :controller => "venues", :action => "create" })
-  get("/update_users/:user_id", { :controller => "users", :action => "update" })
+  get("/", { :controller => "users", :action => "index" })
+  get("/users", { :controller => "users", :action => "index" })
+  get("/users/:username", { :controller => "users", :action => "show" })
+  get("/insert_user_record", { :controller => "users", :action => "create" })
+  get("/update_user/:user_id", { :controller => "users", :action => "update" })
 
-  get("/venues", { :controller => "venues", :action => "index" })
-  get("/insert_venue_record", { :controller => "venues", :action => "create" })
-  get("/venue/:an_id", { :controller => "venue", :action => "show" })
-  post("/update_venue/:the_id", { :controller => "application", :action => "update" })
+  get("/venues", { :controller => "venue", :action => "index" })
+  get("/insert_venue_record", { :controller => "venue", :action => "create" })
+  get("/venues/:an_id", { :controller => "venue", :action => "show" })
+  get("/update_venue/:the_id", { :controller => "venue", :action => "update" })
   get("/delete_venue/:id_to_delete", { :controller => "venue", :action => "destroy" })
   
   get("/insert_comment_record", { :controller => "comments", :action => "create" })
